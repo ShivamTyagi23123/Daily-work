af0ba4090ee853d0a9a8341ebba4a10ee6c2627b
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-10-30 11:11:09 +0530

    Task of Day 30 oct C programing

--- numadd.c
+++ numadd.c
@@ -5,23 +5,26 @@ int main() {
     FILE *file;
     char filename[100] = "/home/shivamtyagi/auto-cpufreq/LICENSE";
     file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Could not open file %s\n", filename);
-        return 1;
-    }
     int sum = 0;
-    int num;
-    while (fscanf(file, "%d", &num) == 1) {
+    int num = 0;
+    int isnum = 0;
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch >= '0' && ch <= '9') {
+            num = num * 10 + (ch - '0');
+            isnum = 1;
+        } else if (isnum) {
+            sum += num;
+            num = 0;
+            isnum = 0;
+        }
+    }
+    if (isnum) {
         sum += num;
     }
     fclose(file);
-
     file = fopen(filename, "a");
-    if (file == NULL) {
-        printf("Could not open file %s\n", filename);
-        return 1;
-    }
-    fprintf(file, "\n%d\n", sum);
+    printf("\n%d\n", sum);
     fclose(file);
     return 0;
 }
