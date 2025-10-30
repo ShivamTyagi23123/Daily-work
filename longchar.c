6e98936a333a0f2884d45ff40b00754eec7a16eb
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-10-30 11:09:47 +0530

    Task of Day 30 oct C programing

--- longchar.c
+++ longchar.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
     FILE *file;
     char filename[100] = "/home/shivamtyagi/auto-cpufreq/LICENSE";
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Could not open file %s\n", filename);
-        return 1;
-    }
-    char longest_word[100];
-    longest_word[0] = '\0';
-    char word[100];
-    while (fscanf(file, "%s", word) == 1) {
-        if (strlen(word) > strlen(longest_word)) {
-            strcpy(longest_word, word);
+    char longest_word[100] = {0};
+    char current_word[100];
+    int len_longest = 0, len_current = 0;
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+            if (len_current < 99) {
+                current_word[len_current++] = (char)ch;
+            }
+        } else {
+            if (len_current > len_longest) {
+                len_longest = len_current;
+                for (int i = 0; i < len_current; i++) {
+                    longest_word[i] = current_word[i];
+                }
+                longest_word[len_longest] = '\0';
+            }
+            len_current = 0;
         }
     }
+    if (len_current > len_longest) {
+        len_longest = len_current;
+        for (int i = 0; i < len_current; i++) {
+            longest_word[i] = current_word[i];
+        }
+        longest_word[len_longest] = '\0';
+    }
+
     printf("Longest word: %s\n", longest_word);
+
     fclose(file);
     return 0;
 }
