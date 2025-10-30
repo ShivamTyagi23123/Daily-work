c1faf7b92ca4dc2e3ecce752bb16c1a90dee5894
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-10-30 11:08:46 +0530

    Task of Day 30 oct C programing

--- displine.c
+++ displine.c
@@ -5,11 +5,7 @@ int main() {
     FILE *file;
     char filename[100] = "/home/shivamtyagi/auto-cpufreq/LICENSE";
     file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Could not open file %s\n", filename);
-        return 1;
-    }
-    char line[256];
+    char line[250];
     while (fgets(line, sizeof(line), file)) {
         printf("%s", line);
     }
